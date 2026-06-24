d07d043c976e65371f2324eac70a2d800021ce99
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-06-24 13:41:21 +0800

    fix: add proper rollback

--- database/migrations/2026_04_18_144426_add_role_to_users_table.php
+++ database/migrations/2026_04_18_144426_add_role_to_users_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['role', 'company_name', 'company_logo', 'company_description', 'phone']);
         });
     }
 };
--- database/migrations/2026_04_18_144504_job_listing_tag_table.php
+++ database/migrations/2026_04_18_144504_job_listing_tag_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('job_listing_tag');
     }
 };
